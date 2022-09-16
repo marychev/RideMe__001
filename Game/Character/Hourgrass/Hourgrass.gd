bd54b476e73cab23af0b50168351fd96f23b71d4
marychev <marychevmihail3737@gmail.com>
2022-09-16 15:56:12 +0300

    Added BounusArea

--- Game/Character/Hourgrass/Hourgrass.gd
+++ Game/Character/Hourgrass/Hourgrass.gd
@@ -1,42 +1,23 @@
-extends Area2D
+extends BonusArea
 class_name Hourgrass
 
-onready var player: Player = get_node(PathData.PATH_PLAYER)
-onready var animation: AnimationPlayer = get_node("AnimationPlayer")
 
-
-func _ready() -> void:
-	set_physics_process(false)
-
-
-func _on_body_entered(body: KinematicBody2D) -> void:
+func _on_body_entered(body: Player) -> void:
+	._on_body_entered(body)
+	
 	if is_instance_valid(body) and body.name == "Player":
-		animation.play("fade_out")
-		player_do_anim_success(body)
 		timeout_do_anim_success()
-		body.get_node('AudioMove').set_stream(body.audio_colected)
-		body.get_node('AudioMove').play()
 		set_value_of_time_level()
-		
 		var res := PlayerData.set_time_level_count(body)
 		assert(res != OK, "ERROR: Hourgrass _on_body_entered set_time_level_count")
 
 
+func _on_body_exited(body: Player):
+	._on_body_exited(body)
+	
+
 func _on_VisibilityNotifier_screen_exited() -> void:
-	queue_free()
-
-
-func _on_Hourgrass_body_shape_entered(
-	body_id: int, body: Node, body_shape: int, local_shape: int
-) -> void:
-	if body_id and body and body_shape and local_shape:
-		player_do_anim_success(body)
-
-
-func player_do_anim_success(_player: KinematicBody2D) -> void:
-	if is_instance_valid(_player):
-		player = _player
-	player.anim_player.play(PlayerData.ANIMATION_SUCCESS)
+	._on_VisibilityNotifier_screen_exited()
 
 
 func timeout_do_anim_success() -> void:
